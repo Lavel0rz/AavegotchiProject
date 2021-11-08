b8563f2d96d29d66aef8467723d1df73e08d4258
Lavel0rz <85870602+Lavel0rz@users.noreply.github.com>
2021-11-08 16:35:18 +0100

    updates

--- functs.py
+++ functs.py
@@ -1,4 +1,4 @@
-
+import requests
 import pandas as pd
 import plotly.express as px
 import streamlit as st
@@ -37,3 +37,53 @@ def prepro(data):
     df['precio']=df['precio'].astype(float)
     df['precio'] = df['precio'].apply(lambda x: x/1000000000000000000)
     return df
+
+def prepro2(data):
+    newdic = data.get('data',0)
+    newdic2 = newdic.get('erc721Listings')
+    nuevos = []
+    new = []
+    new2 = []
+    new3 = []
+    new4 = []
+    for i in newdic2:
+        nuevos.append(i)
+    for i in nuevos:
+        new.append(i.get('priceInWei'))
+        new2.append(i.get('district'))
+        new3.append(i.get('size'))
+        new4.append(i.get('id'))
+    df = pd.DataFrame({'precio':new,
+                 'distrito':new2,
+                 'tamaño':new3,
+                 'BazaarID':new4})
+    df['precio']=df['precio'].astype(float)
+    df['precio'] = df['precio'].apply(lambda x: x/1000000000000000000)
+    return df
+
+def districtfloors(df2,D,Size):
+    if Size == 'Humble':
+        Size = 0
+    elif Size == 'Reasonable':
+        Size = 1
+    elif Size == 'Vertical Spacious':
+        Size = 2
+    else:
+        Size = 3
+
+    df2['distrito']=df2['distrito'].astype(int)
+    df2['tamaño'] = df2['tamaño'].astype(int)
+    df3 = df2[df2['distrito']==D]
+    df4 = df3[df3['tamaño']==Size]
+    grouped = df4.groupby(['precio'])['BazaarID'].min()
+    x = (grouped.values[0])
+    return st.text('https://aavegotchi.com/baazaar/erc721/'+str(x))
+
+def run_query(data):
+    request = requests.post('https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-core-matic'
+                            '',
+                            json={'query': data})
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception('Query failed. return code is {}.      {}'.format(request.status_code, data))
--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 import streamlit as st
 import time
-from functs import grafico,prepro
-import requests
+from functs import grafico,prepro,prepro2,districtfloors,run_query
 
+import pandas as pd
 
 root_path = "https://thegraph.com/hosted-service/subgraph/aavegotchi/aavegotchi-core-matic?Aavegotchi?id:1!"
-
-
-def run_query(q):
-    request = requests.post('https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-core-matic'
-                            '',
-                            json={'query': query})
-    if request.status_code == 200:
-        return request.json()
-    else:
-        raise Exception('Query failed. return code is {}.      {}'.format(request.status_code, query))
 
 
 query = '''
@@ -34,15 +24,64 @@ query = '''
 
 
 '''
+query2= '''
+{
+  erc721Listings (orderBy:tokenId,first:1000,where:{category:4,timePurchased:0,cancelled:false}) {
+         id
+    category
+    priceInWei
+    size
+    timePurchased
+    district
+    parcel {
+               id 
+             }
+  }
+}
+
+
+
+'''
+query3 = '''
+{
+  erc721Listings (orderBy:tokenId,first:1000,skip:1000,where:{category:4,timePurchased:0,cancelled:false}) {
+         id
+    category
+    priceInWei
+    size
+    timePurchased
+    district
+    parcel {
+               id 
+             }
+  }
+}
+
+
+
+'''
+result2 = run_query(query2)
+result3 = run_query(query3)
 result = run_query(query)
+df1 = prepro2(result2)
+df2 = prepro2(result3)
+df3 = pd.concat([df1, df2])
 df = prepro(result)
 st.set_page_config(page_title="Aavegotchi", page_icon="money", layout='wide', initial_sidebar_state='auto')
 
-option = st.sidebar.selectbox('Home',['Districts Visualizer'])
+option = st.sidebar.selectbox('Home',['Districts Visualizer','Floor Sniper'])
 if option == 'Districts Visualizer':
     st.title('Aavegotchi Parcels Average Bazaar Prices By District')
 
-district = st.select_slider('Select District', [1,2,3,4,5,14,15,16,17,18,19,20,21,22,39,40,41,42,43])
+    district = st.select_slider('Select District', [1,2,3,4,5,14,15,16,17,18,19,20,21,22,39,40,41,42,43])
 
-grafico(df,district)
-time.sleep(2)
+    grafico(df,district)
+    time.sleep(2)
+
+if option == 'Floor Sniper':
+    D = st.selectbox('Choose District',(1,2,3,4,5,14,15,16,17,18,19,20,21,22,39,40,41,42,43))
+    Size = st.selectbox('Choose Size',('Humble','Reasonable','Vertical Spacious','Horizontal Spacious'))
+    print(df3)
+    districtfloors(df3,D,Size)
+    time.sleep(2)
+
