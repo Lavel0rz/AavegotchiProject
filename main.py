1703cd2ea2147c8dd95880284675419cbaac7b23
Lavel0rz <85870602+Lavel0rz@users.noreply.github.com>
2021-11-18 21:12:27 +0100

    smol update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as stream_lit
 from view.view_serializer import ViewSerializer
-
+import streamlit as st
 
 ROOT_PATH = "https://thegraph.com/hosted-service/subgraph/aavegotchi/aavegotchi-core-matic?Aavegotchi?id:1!"
 
@@ -9,7 +9,7 @@ stream_lit.set_page_config(page_title="Aavegotchi", page_icon="money", layout='w
 option = stream_lit.sidebar.selectbox('Home',
                                       ['HOME', 'Districts Visualizer', 'Floor Sniper', 'Price Estimator',
                                        'Neighbour Parcels',
-                                       'Bazaar Stats','Wearable Floors Overview'])
+                                       'Bazaar Stats','Wearable Floors Overview','Donate'])
 
 view_serializer = ViewSerializer(stream_lit)
 
@@ -34,3 +34,6 @@ if option == 'Bazaar Stats':
 
 if option == 'Wearable Floors Overview':
     view_serializer.render_floor_sniper_wearables()
+
+if option == 'Donate':
+    st.text('If you feel like supporting my work you can donate so I can keep updating this web app! 0xbC44A93bF334e872f1cf004Bb0Ccde28E8fCb8Ea , GHST or MATIC appreciated!')
